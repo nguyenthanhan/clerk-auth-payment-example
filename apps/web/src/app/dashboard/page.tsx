1b3fc5acc21283e044095214a7bb20c0ca56c3f6
Heimer Nguyen <heimer@vinova.com.sg>
2025-09-05 16:36:26 +0700

    feat: update TypeScript configuration and enhance Dashboard component with PricingTable

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,34 +1,35 @@
 "use client";
 
 import { api } from "@clerk-auth-payment-example/backend/convex/_generated/api";
-import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import { PricingTable, SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import {
-	Authenticated,
-	AuthLoading,
-	Unauthenticated,
-	useQuery,
+  Authenticated,
+  AuthLoading,
+  Unauthenticated,
+  useQuery,
 } from "convex/react";
 
 export default function Dashboard() {
-	const user = useUser();
-	const privateData = useQuery(api.privateData.get);
+  const user = useUser();
+  const privateData = useQuery(api.privateData.get);
 
-	return (
-		<>
-			<Authenticated>
-				<div>
-					<h1>Dashboard</h1>
-					<p>Welcome {user.user?.fullName}</p>
-					<p>privateData: {privateData?.message}</p>
-					<UserButton />
-				</div>
-			</Authenticated>
-			<Unauthenticated>
-				<SignInButton />
-			</Unauthenticated>
-			<AuthLoading>
-				<div>Loading...</div>
-			</AuthLoading>
-		</>
-	);
+  return (
+    <>
+      <Authenticated>
+        <div>
+          <h1>Dashboard</h1>
+          <p>Welcome {user.user?.fullName}</p>
+          <p>privateData: {privateData?.message}</p>
+          <UserButton />
+          <PricingTable />
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+      <AuthLoading>
+        <div>Loading...</div>
+      </AuthLoading>
+    </>
+  );
 }
